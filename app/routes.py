d1726cc705c472a9bb5b78b7d5dfaab2f7f62d17
Kevin Santana <klinarte258@gmail.com>
2024-07-16 13:16:45 -0600

    Html iniciado

--- app/routes.py
+++ app/routes.py
@@ -1,44 +1,16 @@
 from flask import request, render_template, redirect, url_for, current_app as app
+from datetime import datetime
+# from business import BusinessLogic
+# from entities import User
 
-from business import BusinessLogic
-from entities import User
 
+# business_logic = BusinessLogic()
 
-business_logic = BusinessLogic()
+inicio = ''
+date = datetime.now().date()
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/submit',methods = ['POST'])
-def submit():
-    name = request.form['name']
-    user = User (name = name)
-
-    result = business_logic.process_user(user)
-    return render_template ('result.html', result = result)
-
-@app.route('/users')
-def users():
-    all_users = business_logic.get_all_users()
-    return render_template('users.html', users=all_users)
-
-@app.route('/edit/<int:user_id>', methods=['GET', 'POST'])
-def edit_user(user_id):
-    if request.method == 'POST':
-        user = User()
-        user.name = request.form['name']
-        user.id = request.form['id']
-        result = business_logic.update_user(user)
-        return redirect(url_for('users'))
-    else:
-        all_users = business_logic.get_all_users()
-        user = next((u for u in all_users if u['id'] == user_id), None)
-        return render_template('edit_user.html', user=user) 
-
-@app.route('/delete/<int:user_id>', methods=['POST'])
-def deleted_user(user_id):
-    userId = request.form['user_id']
-    result = business_logic.deleteUser(userId)
-    return render_template('deleted.html')
+    inicio = True
+    return render_template('index.html', inicio = inicio, date = date)
 
